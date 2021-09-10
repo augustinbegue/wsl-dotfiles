2718a51ab2d6fb82aa51136863613742484994d2
Augustin Bégué <augustin.begue@epita.fr>
2021-09-10 08:51:37 +0200

    packages in install script

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
+# install packages
+sudo pacman -Syy
+sudo pacman -S --needed git base-devel curl
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ~
+yay -S zsh zsh-completions sdl2 make gcc
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 git clone --bare git@github.com:augustinbegue/wsl-dotfiles.git $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
